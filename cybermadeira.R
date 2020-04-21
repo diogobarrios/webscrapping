fd596e86c4a3cf49af72213b0aca70f1eb783033
diogobarrios <diogobarrios22@gmail.com>
2020-04-22 00:49:56 +0100

    " doing some changes"

--- cybermadeira.R
+++ cybermadeira.R
@@ -2,39 +2,54 @@ library("rvest")
 library("dplyr")
 library("stringr")
 library("purrr")
-cyberTelefonia <- "http://www.cybermadeira.com/classificados-madeira_1_telefonia_23_1.html"
+library("plyr")
+
+cyberTelefonia <- "http://www.cybermadeira.com/telefonia.html?page=1.html"
 cyber_telefonia <- read_html(cyberTelefonia)
 str(cyber_telefonia)
+
 
 cyberBody_nodes <- cyber_telefonia %>%
   html_nodes("body") %>%
   html_children()
 cyberBody_nodes
 
+
 cyberTitle <- cyber_telefonia %>%
   rvest::html_nodes('body') %>% 
-  xml2::xml_find_all("//a[contains(@class, 'title')]") %>% 
-  rvest::html_text()
+  xml2::xml_find_all("//h3[contains(@class, 'text-secondary')]") %>%
+  rvest::html_text() %>%
+  str_trim() %>%
+  str_extract(pattern = ".*")
+
 
 cyberPrice <- cyber_telefonia %>%
   rvest::html_nodes('body') %>% 
-  xml2::xml_find_all("//a[contains(@class, 'listPrice price text-right')]") %>% 
-  rvest::html_text()
+  xml2::xml_find_all("//span[contains(@class, 'text-danger font-weight-bolder')]") %>% 
+  rvest::html_text() %>%
+  str_extract(pattern = "^[0-9]+")
 
 cyberCity <- cyber_telefonia %>%
   rvest::html_nodes('body') %>% 
-  xml2::xml_find_all("//a[contains(@class, 'city')]") %>% 
+  xml2::xml_find_all("//span[contains(@class, 'text-grey')]") %>% 
+  rvest::html_text() 
+  #str_split(pattern = ",")
+cyberCity
+cyberDescriptive <- cyber_telefonia %>%
+  rvest::html_nodes('body') %>% 
+  xml2::xml_find_all("//span[contains(@class, 'text-dark')]") %>% 
   rvest::html_text()
 
 cyberDate <- cyber_telefonia %>%
   rvest::html_nodes('body') %>% 
-  xml2::xml_find_all("//span[contains(@class, 'col-sm-3 col-xs-6 c3')]") %>% 
+  xml2::xml_find_all("//div[contains(@class, 'col-6 col-sm-3 text-center text-dark')]") %>% 
   rvest::html_text() %>%
   str_extract("[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9]") %>%
-  as.Date() %>%
-  na.omit()
+  na.exclude() %>%
+  as.Date("%d/%m/%Y") 
+cyberDate
 
-cyber_df <- data.frame(cyberTitle, cyberPrice, cyberCity, cyberDate)
+cyber_df <- data.frame(cyberDate, cyberTitle, cyberPrice, cyberCity, cyberDescriptive)
 View(cyber_df)
 
 knitr::kable(
