c1cb6e58914c8ff915b6b00465d82409db35431f
Diogo Barrios <diogo_barr@hotmail.com>
2021-05-28 15:57:18 +0100

    new example v21.05.28

--- web_scraper_example.R
+++ web_scraper_example.R
@@ -1,1 +1,7 @@
 # Starting over again to understand these technique.
+# Based from Automated Data Collection with R
+
+# Example HTML readlines()
+url <- "http://www.r-datacollection.com/materials/html/fortunes.html"
+fortunes <- readLines(con = url)
+fortunes
