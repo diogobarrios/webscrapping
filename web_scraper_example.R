2f2b5c28ea1c89ef401dbe15a5a0c046c3b53038
Diogo Barrios <diogo_barr@hotmail.com>
2021-05-28 15:58:49 +0100

    v28.05.21.1

--- web_scraper_example.R
+++ web_scraper_example.R
@@ -5,3 +5,26 @@
 url <- "http://www.r-datacollection.com/materials/html/fortunes.html"
 fortunes <- readLines(con = url)
 fortunes
+
+library("XML") # Parse HTML file to DOM
+parsed_fortunes <- htmlParse(file = url)
+print(parsed_fortunes)
+
+# creating handlers
+h1 <- list("body" = function(x) {NULL})
+parsed_fortunes <- htmlTreeParse(url, handlers = h1, asTree = T)
+parsed_fortunes$children
+
+# creating handlers to delete all nodes with name div or title as well as comments
+h2 <- list(
+        startElement = function(node, ...){
+          name <- xmlName(node)
+          if(name %in% c("div", "title")) {NULL}
+          else 
+          {node}},
+          comment = function(node){NULL}
+)
+
+
+parsed_fortunes <- htmlTreeParse(url, handlers = h2, asTree = T)
+parsed_fortunes$children
